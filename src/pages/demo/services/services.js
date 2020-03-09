252fb4a49f7690d4caa4e7b84550e782bdcd2fc5
luodantong <luo.dantong2@iwhalecloud.com>
2020-03-09 09:54:24 +0800

    test

--- src/pages/demo/services/services.js
+++ src/pages/demo/services/services.js
@@ -3,6 +3,6 @@ import { post, get } from "utils/request";
 /** 接口描述
  *@param {string} param
  */
-const apiName = ({ param }) => post(`/path`, { param });
+const apiName = ({ param }) => post(`/path1`, { param });
 
 export default { apiName };
